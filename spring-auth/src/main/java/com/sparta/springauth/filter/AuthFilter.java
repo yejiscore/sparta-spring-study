f4871b05a7eed791f2b050dc3ad87d8d8e575f9b
yejiscore <yejisong24@gmail.com>
2025-02-07 14:39:25 +0900

    Spring Security Login

--- spring-auth/src/main/java/com/sparta/springauth/filter/AuthFilter.java
+++ spring-auth/src/main/java/com/sparta/springauth/filter/AuthFilter.java
@@ -14,7 +14,7 @@ import org.springframework.util.StringUtils;
 import java.io.IOException;
 
 @Slf4j(topic = "AuthFilter")
-@Component
+//@Component // Spring Security 인증 기능 제외
 @Order(2)
 public class AuthFilter implements Filter {
 
