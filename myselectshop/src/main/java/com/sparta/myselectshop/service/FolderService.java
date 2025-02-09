efbe9e612516ede253730768bd8365ea7ed75bf5
yejiscore <yejisong24@gmail.com>
2025-02-10 06:03:33 +0900

    상품 폴더 설계

---
+++ myselectshop/src/main/java/com/sparta/myselectshop/service/FolderService.java
@@ -0,0 +1,20 @@
+package com.sparta.myselectshop.service;
+
+import com.sparta.myselectshop.entity.Folder;
+import com.sparta.myselectshop.entity.User;
+import com.sparta.myselectshop.repository.FolderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FolderService {
+
+    private final FolderRepository folderRepository;
+
+    public void addFolders(List<String> folderNames, User user) {
+        List<Folder> existFolderList = folderRepository.findAllByUserAndNameIn(user, folderNames);
+    }
+}
