97ecaef15f14d446131b591f2750048a3bf143e8
yejiscore <yejisong24@gmail.com>
2025-02-10 07:01:39 +0900

    관심 상품에 폴더 추가 및 폴더 전체 조회 구현

--- myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -10,8 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -38,5 +36,14 @@ public class ProductController {
 
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return productService.getProducts(userDetails.getUser(), page-1, size, sortBy, isAsc);
+    }
+
+    @PostMapping("/products/{productId}/folder")
+    public void addFolder(
+            @PathVariable Long productId,
+            @RequestParam Long folderId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        productService.addFolder(productId, folderId, userDetails.getUser());
     }
 }
